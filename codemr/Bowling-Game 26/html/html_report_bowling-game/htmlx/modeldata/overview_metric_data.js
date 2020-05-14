c208ed481faa92b1a91137bc361e25a1f8e3ce8f
arpan-dasgupta <arpandasgupta0@gmail.com>
2020-05-14 23:49:37 +0530

    Cleanup unnecessary dirs

--- codemr/Bowling-Game 26/html/html_report_bowling-game/htmlx/modeldata/overview_metric_data.js
+++
@@ -1,1 +0,0 @@
-var CodeMR_ProjectMetrics = {"Project Name":"Bowling-Game working set: {Bowling-Game}", "Number of Highly Problematic Classes":0, "Number of Entities":36, "Number of Problematic Classes":1, "Class Lines of Code":1822, "Number of External Packages":13, "Number of Packages":1, "Number of External Entities":69}
